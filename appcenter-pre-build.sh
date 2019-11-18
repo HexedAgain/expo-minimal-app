39479bae28e64f4ce69896db650855d13c4b5a26
Greg Astley <gregory.astley@gmail.com>
2019-11-18 22:04:19 +0000

    add test release channel

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -6,4 +6,4 @@ sudo npm install -g expo-cli --unsafe-perm=true --allow-root
 echo "EXPO - Login"
 expo login -u gregastley -p $EXPO_PWD
 
-expo publish --release-channel uat
+expo publish --release-channel "test"
