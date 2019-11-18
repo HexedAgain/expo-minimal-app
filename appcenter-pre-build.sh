c38abe5be32d6da3fc8fe0ce12dd48ce573089fa
Greg Astley <gregory.astley@gmail.com>
2019-11-18 20:41:15 +0000

    update username

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,6 +4,6 @@ echo "EXPO - Install globally"
 sudo npm install -g expo-cli --unsafe-perm=true --allow-root
 
 echo "EXPO - Login"
-expo login -u gregory.astley@gmail.com -p $EXPO_PWD
+expo login -u gregastley -p $EXPO_PWD
 
 expo publish
