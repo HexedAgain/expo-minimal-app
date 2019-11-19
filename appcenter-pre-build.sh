38d770f743117058f09334d3ae9f23780fffca2f
Greg Astley <gregory.astley@gmail.com>
2019-11-19 00:20:23 +0000

    test expo start

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -6,4 +6,8 @@ sudo npm install -g expo-cli --unsafe-perm=true --allow-root
 echo "EXPO - Login"
 expo login -u gregastley -p $EXPO_PWD
 
+echo "EXPO - Start"
+echo start
+
+echo "EXPO - Publish"
 expo publish --release-channel "test"
